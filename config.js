b49ad3dc0722e52a3d9e4c1ec4c525d0cc19d0db
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2025-01-14 14:39:26 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const apiKey = 'AIzaSyBbRnH6zZsKyQMbxIg7thGKCappZsJl5GI';
-const chatStorageKey = 'chatHistory';
-const pointsStorageKey = 'userPoints';
-const pointsPerAd = 10;
-
-// يمكنك إضافة المزيد من المتغيرات الحساسة هنا
